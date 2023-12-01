10489c3120fd4574fa2f4d8ad5c05d8dc916304f
Liang Chen <liangyu.chen.dev@gmail.com>
2023-11-30 22:40:15 -0500

    base socket connect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+const io = require("socket.io-client");
+
+const socket = io.connect("http://localhost:9000");
 
 function App() {
+  const sendMessage = () => {
+    console.log("send message!")
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder='message'></input>
+      <button onClick={sendMessage}>submit</button>
     </div>
   );
 }
