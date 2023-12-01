65d6daff7f819ec745215a1aa74cebb1b13dd7a1
Liang Chen <liangyu.chen.dev@gmail.com>
2023-11-30 23:03:17 -0500

    send simple msg

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-const io = require("socket.io-client");
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-const socket = io.connect("http://localhost:9000");
+const socket = io.connect('http://localhost:9000'); // Connect to your server
 
 function App() {
+  const [message, setMessage] = useState('');
+
   const sendMessage = () => {
-    console.log("send message!")
+    // Send a message to the server
+    socket.emit('message', message);
+    setMessage('');
   };
+
   return (
-    <div className="App">
-      <input placeholder='message'></input>
-      <button onClick={sendMessage}>submit</button>
+    <div>
+      <h1>Socket.io React App</h1>
+      <div>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
 }
